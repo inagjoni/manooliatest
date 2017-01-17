587a5ffdc5807db919bdea7d16a79507aaf53575
Ina Gjoni <ina.gjoni01@gmail.com>
2017-01-17 20:02:36 +0100

    symfony && fos user

---
+++ src/AppBundle/Controller/DefaultController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function indexAction(Request $request)
+    {
+        // replace this example code with whatever you need
+       /* return $this->render('default/index.html.twig', array(
+            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
+        ));*/
+         return $this->render('default/home.html.twig', array(        ));
+    }
+
+    /**
+     * @Route("/test", name="homepage2")
+     */
+    public function homeAction(Request $request)
+    {
+        // replace this example code with whatever you need
+       /* return $this->render('default/index.html.twig', array(
+            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
+        ));*/
+         return $this->render('default/home.html.twig', array( 'text' => $request  ));
+    }
+}
---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use FOS\UserBundle\Entity\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * User
+ *
+ * @ORM\Table(name="user")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\UserRepository")
+ */
+class User extends BaseUser
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+
+    public function __construct()
+    {
+        parent::__construct();
+        // your own logic
+    }
+}
+
